014aa5bc2d23ccd7f9d18bc4641df022ef8a6064
Graciela Nouga <gracielaruthcarolenouga@gmail.com>
2025-05-18 22:41:47 -0400

    Implémentation partielle des classes InventoryDatabase et InventoryDatabaseNode

--- src/Inventaire/InventoryDatabase.java
+++ src/Inventaire/InventoryDatabase.java
@@ -1,4 +1,37 @@
 package Item.Inventaire;
 
+import Item.Item.Item;
+
 public class InventoryDatabase {
+    private int itemsCount ;
+    private InventoryDatabaseNode first ;
+
+    public InventoryDatabase(){
+
+    }
+
+    public void insert(Item item){
+        InventoryDatabaseNode newItem = new InventoryDatabaseNode(item) ;
+        this.getLast().next = newItem ;
+    }
+
+    private InventoryDatabaseNode getLast(){
+        InventoryDatabaseNode actual = first ;
+        while (actual.next != null){
+            actual = actual.next ;
+        }
+        return actual ;
+    }
+
+    public Item findByID(int ID){
+        return null ;
+    }
+
+    public void remove(int ID){
+
+    }
+
+    public Item[] getArrayOfItems(){
+        return null;
+    }
 }
--- src/Inventaire/InventoryDatabaseNode.java
+++ src/Inventaire/InventoryDatabaseNode.java
@@ -1,4 +1,13 @@
 package Item.Inventaire;
 
+import Item.Item.Item;
+
 public class InventoryDatabaseNode {
+    private Item item ;
+    public InventoryDatabaseNode next ;
+
+    public InventoryDatabaseNode(Item item){
+        this.item = item ;
+        this.next = null ;
+    }
 }
